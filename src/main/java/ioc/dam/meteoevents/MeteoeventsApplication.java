c7525db2b8c82e6528b9f742425a9131c184a51b
roberthg93 <roberthg93@gmail.com>
2024-10-03 10:42:13 +0200

    Add files via upload

---
+++ src/main/java/ioc/dam/meteoevents/MeteoeventsApplication.java
@@ -0,0 +1,13 @@
+package ioc.dam.meteoevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeteoeventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeteoeventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ioc/dam/meteoevents/MeteoeventsApplicationTests.java
@@ -0,0 +1,13 @@
+package ioc.dam.meteoevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeteoeventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
